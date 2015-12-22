3d947bbc3c3a386c3b10bf794d546a2e8e5a2774
Scott González <scott.gonzalez@gmail.com>
2015-12-22 14:19:35 -0500

    Add uniqueness check for player names

--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -1,20 +1,27 @@
 require 'test_helper'
 
 class PlayerTest < ActiveSupport::TestCase
-  test "should get error creating player with no name" do
+  test "should require name" do
     player = Player.new
-    player.save
-
-    # Player's name is nil - should fail
-    assert(player.errors[:name].any?)
+    assert_equal(player.save, false, "Should not save")
+    assert_equal(player.errors[:name], ["can't be blank"],
+      "Should provide error message for name")
   end
 
-  test "should create valid player" do
-    player = Player.new
-    player.name = "Test Player"
-    player.save
+  test "should create player" do
+    player = Player.new(name: "John")
+    assert_equal(player.save, true, "Should save player")
+    assert_equal(player.wins, 0, "Should default to 0 wins")
+    assert_equal(player.losses, 0, "Should default to 0 losses")
+  end
 
-    # Player has a name, it should be validated
-    assert(player.errors[:name].none?)
+  test "should require unique name" do
+    player1 = Player.new(name: "John")
+    assert_equal(player1.save, true, "Should save first player")
+
+    player2 = Player.new(name: "John")
+    assert_equal(player2.save, false, "Should not save second player");
+    assert_equal(player2.errors[:name], ["has already been taken"],
+      "Should provide error message for name")
   end
 end
