3605a199ce8735e77400a5f660a8ae78965c6497
Kyle Pollich <kpollich1@gmail.com>
2015-12-22 12:07:40 -0500

    Test: Investigate testing in Rails.

--- test/controllers/players_controller_test.rb
+++ test/controllers/players_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class PlayersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil :players
+  end
+
+  test "should show player" do
+    get(:show, { "id" => "1" })
+    assert_response :success
+    assert_not_nil assigns(:player)
+    assert_not_nil assigns(:history)
+  end
+
+  test "should create player" do
+    assert_difference("Player.count") do
+      post :create, player: {name: "New Player"}
+    end
+    assert_redirected_to players_path
+  end
 end
--- test/models/match_test.rb
+++ test/models/match_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class MatchTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "winners" do
+    assert_equal(Match.find(1).winner, Player.find(1), "Player one should win match one!")
+    assert_equal(Match.find(2).winner, Player.find(3), "Player three should win match two!")
+  end
+
+  test "losers" do
+    assert_equal(Match.find(1).loser, Player.find(2), "Player two should lose match one!")
+    assert_equal(Match.find(2).loser, Player.find(4), "Player four should lose match two!")
+  end
 end
